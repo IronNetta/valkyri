716c2b0d2434dfaac255d814816ecd5bc1702365
Sebastien De Laet <delaet.sebastien93@gmail.com>
2025-03-26 11:13:45 +0100

    ajout page d'accueil dynamique

--- src/app/features/auth/services/auth-service.ts
+++ src/app/features/auth/services/auth-service.ts
@@ -13,27 +13,15 @@ export class AuthService {
 
   private readonly _http: HttpClient = inject(HttpClient);
 
-  // private readonly _currentUser$: BehaviorSubject<UserTokenDto|undefined>;
   currentUser: WritableSignal<UserTokenDto|undefined>;
+
 
   constructor() {
 
     let jsonUser = localStorage.getItem('currentUser');
     this.currentUser = signal(jsonUser ? JSON.parse(jsonUser) : undefined);
 
-    // this._currentUser$ = new BehaviorSubject<UserTokenDto|undefined>(undefined);
-    // if(jsonUser) {
-    //   this._currentUser$.next(JSON.parse(jsonUser));
-    // }
   }
-
-  // get currentUser() {
-  //   return this._currentUser$?.value;
-  // }
-  //
-  // get currentUser$() {
-  //   return this._currentUser$?.asObservable();
-  // }
 
   register(form: RegisterFormModel) {
     return this._http.post<void>(`${environment.API_URL}/register`,form);
@@ -52,6 +40,6 @@ export class AuthService {
   logout() {
     localStorage.removeItem("currentUser");
     this.currentUser.set(undefined);
-    // this._currentUser$?.next(undefined);
   }
+
 }
