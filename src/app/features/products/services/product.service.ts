6969c5b2122d84b71af4f7d68d8379b49150d2e7
IronNetta <delaet.sebastien22@gmail.com>
2025-03-31 15:56:01 +0200

    correction

--- src/app/features/products/services/product.service.ts
+++ src/app/features/products/services/product.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {ProductCreateFormModel} from '../models/product-create-form.model';
 import {environment} from '../../../../environments/environment';
 import {ProductDtoModel} from '../models/product-dto.model';
+import {ProductDetailsDtoModel} from '../models/product-details-dto.model';
 
 @Injectable({
   providedIn: 'root'
@@ -17,19 +18,19 @@ export class ProductService {
     return this._http.get<ProductDtoModel[]>(`${environment.API_URL}/product`);
   }
 
-  public getProduct(id: number) {
-    return this._http.get<ProductDtoModel>(`${environment.API_URL}/product/${id}`);
+  public findByID(id: number) {
+    return this._http.get<ProductDetailsDtoModel>(`${environment.API_URL}/products/${id}`);
   }
 
   public deleteProduct(id: number) {
     return this._http.delete<void>(`${environment.API_URL}/product/${id}`);
   }
 
-  public createProduct(product: ProductCreateFormModel) {
-    return this._http.post<void>(`${environment.API_URL}/product`, product);
+  public create(product: ProductCreateFormModel) {
+    return this._http.post<ProductDetailsDtoModel>(environment.API_URL + '/products', product);
   }
 
-  public updateProduct(id: number, product: ProductCreateFormModel) {
-    return this._http.put<void>(`${environment.API_URL}/product/${id}`, product);
+  public update(product: ProductCreateFormModel) {
+    return this._http.put<ProductDetailsDtoModel>(environment.API_URL + '/products', product);
   }
 }
