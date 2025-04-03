6665a25b79a0291352539f05ed7d82b3d3a16215
Thibault Delaunois <thibaultdelaunois@gmail.com>
2025-04-03 09:37:55 +0200

    lowStock Functionnal (cannot get data if admin, only commercials can get data)

--- src/app/features/stock/pages/low-stock/low-stock.component.ts
+++ src/app/features/stock/pages/low-stock/low-stock.component.ts
@@ -3,41 +3,34 @@ import {StockService} from '../../services/stock.service';
 import {StockDetailsDto} from '../../models/stock-details-dto.model';
 import {Router} from '@angular/router';
 import {Button} from 'primeng/button';
-import {NgClass} from '@angular/common';
 import {PrimeTemplate} from 'primeng/api';
 import {TableModule} from 'primeng/table';
-import {Paginator} from 'primeng/paginator';
 import {ProductDtoModel} from '../../../products/models/product-dto.model';
 import {ProductService} from '../../../products/services/product.service';
+import { BadgeModule } from 'primeng/badge';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-low-stock',
   imports: [
     Button,
-    NgClass,
     PrimeTemplate,
     TableModule,
-    Paginator
+    BadgeModule,
+    NgStyle
   ],
   templateUrl: './low-stock.component.html',
   styleUrl: './low-stock.component.scss'
 })
 export class LowStockComponent {
-  nomProduit: string = '';
   products = signal<ProductDtoModel[]>([]);
   stocks = signal<StockDetailsDto[]>([]);
-  paginatedStocks = signal<StockDetailsDto[]>([]);
-  itemsPerPage = signal<number>(5);
-  currentPage = signal<number>(0);
   private readonly _service: StockService = inject(StockService);
 
   constructor(
     private router: Router,
     private productService: ProductService,
   ) {
-    effect(() => {
-      this.paginate({ first: 0, rows: this.itemsPerPage() });
-    });
     this.fetchStocks();
 
   }
@@ -46,13 +39,20 @@ export class LowStockComponent {
     this._service.getLowStock().subscribe((data) => {
       this.stocks.set(data);
     });
-    this.paginate({ first: 0, rows: this.itemsPerPage() });
   }
 
-  paginate(event: any): void {
-    const start = event.first;
-    const end = start + event.rows;
-    this.paginatedStocks.set(this.stocks().slice(start, end));
+
+  rowStyle(stock: StockDetailsDto) {
+    if (stock.quantiteDisponible === 0) {
+      return { fontWeight: 'bold', fontStyle: 'italic' };
+    }
+    return {fontStyle: 'normal' };
+  }
+
+  stockSeverity(stock: StockDetailsDto) {
+    if (stock.quantiteDisponible === 0) return 'danger';
+    else if (stock.quantiteDisponible > 0 && stock.quantiteDisponible < 5) return 'warn';
+    else return 'success';
   }
 
   returnToStocks(): void {
